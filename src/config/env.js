194dfe1e05fd4c6bbbf78b5f3c5fe4d2fb23ea0e
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-12-11 20:59:40 +0200

    🔧 chore(.env): Update environment variables with default values and validation

--- src/config/env.js
+++ src/config/env.js
@@ -7,24 +7,27 @@ const envSchema = z.object({
 		.default("development"),
 	DATABASE_URL: z
 		.url("Field should be a database uri")
-		.min(1, { message: "Field cannot be empty" }),
+		.min(1, { error: "Field cannot be empty" }),
 	PORT: z.preprocess((val) => Number(val), z.number()).default(3000),
 	API_VERSION: z.string().default("v1"),
 	FRONTEND_URL: z
 		.url("Field should be a url")
-		.min(1, { message: "Field cannot be empty" }),
+		.min(1, { error: "Field cannot be empty" }),
 	ALLOWED_ORIGINS: z
 		.string()
-		.min(1, { message: "Field cannot be empty" })
+		.min(1, { error: "Field cannot be empty" })
 		.transform(parseIntoArray),
-	JWT_ACCESS_SECRET: z.string().min(1, { message: "Field cannot be empty" }),
+	JWT_ACCESS_SECRET: z.string().min(1, { error: "Field cannot be empty" }),
 	JWT_ACCESS_EXPIRY: z.string().default("15m"),
-	JWT_REFRESH_SECRET: z.string().min(1, { message: "Field cannot be empty" }),
+	JWT_REFRESH_SECRET: z.string().min(1, { error: "Field cannot be empty" }),
 	JWT_REFRESH_EXPIRY: z.string().default("7d"),
-	GMAIL_HOST: z.string().min(1, { message: "Field cannot be empty" }),
-	GMAIL_USER: z.email().min(1, { message: "Field cannot be empty" }),
-	GMAIL_PASSWORD: z.string().min(1, { message: "Field cannot be empty" }),
+	GMAIL_HOST: z.string().min(1, { error: "Field cannot be empty" }),
+	GMAIL_USER: z.email().min(1, { error: "Field cannot be empty" }),
+	GMAIL_PASSWORD: z.string().min(1, { error: "Field cannot be empty" }),
 	EMAIL_VERIFICATION_EXPIRY: z.string().default("5m"),
+	CLOUDINARY_CLOUD_NAME: z.string().min(1, { error: "Field cannot be empty" }),
+	CLOUDINARY_API_KEY: z.string().min(1, { error: "Field cannot be empty" }),
+	CLOUDINARY_API_SECRET: z.string().min(1, { error: "Field cannot be empty" }),
 });
 
 const parsed = envSchema.safeParse(process.env);
